68a370005065e3b4d3dc8b352dcc8d991e007074
Bastien Andre <bastienandre68@gmail.com>
2023-03-01 17:33:00 +0100

    ajout eventlistener boutons filtres et création page login

--- FrontEnd/assets/script.js
+++ FrontEnd/assets/script.js
@@ -1,3 +1,10 @@
+const btnTousEl = document.getElementById("btnTous");
+const btnObjEl = document.getElementById("btnObj");
+const btnAppartEl = document.getElementById("btnAppart");
+const btnHotelEl = document.getElementById("btnHotel");
+
+
+
 fetch("http://localhost:5678/api/works")
     .then(reponse => reponse.json())
     .then(data => {
@@ -18,7 +25,22 @@ fetch("http://localhost:5678/api/works")
             galerieEL.appendChild(NewFigure);
         }
         
-    })
+    });
+
+btnTousEl.addEventListener("click", function(){
+    
+});
 
 
+btnObjEl.addEventListener("click", function(){
+    
+});
+
+btnAppartEl.addEventListener("click", function(){
+
+});
+
+btnHotelEl.addEventListener("click", function(){
+
+});
 
