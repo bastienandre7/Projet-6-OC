39e07cbc690268d7fa1234cd3c6dc9677fa5f1c9
Bastien Andre <bastienandre68@gmail.com>
2023-03-09 16:42:36 +0100

    fetch post (login) terminé

--- FrontEnd/assets/login.js
+++ FrontEnd/assets/login.js
@@ -1,23 +1,31 @@
 const form = document.getElementById('form');
 
 
-form.addEventListener('submit', function(e){
+
+form.addEventListener('submit', async function(e){
     e.preventDefault();
 
-    const user = new FormData(form);
-    const res = Object.fromEntries(user);
+    const data = new FormData(form);
+    const response = await login(data);
+    const user = await response.json();
+    console.log(user);
 
-    console.log([...user]);
 
-    fetch('http://localhost:5678/api/users/login', {
+});
+
+const login = async (data) =>{
+
+    const user = {
+        email: data.get('email'),
+        password: data.get('password')
+    };
+
+    return await fetch('http://localhost:5678/api/users/login', {
         method: "POST",
         headers: {
             'accept': 'application/json',
             'Content-Type': 'application/json',
         },
-        body: JSON.stringify(res)
+        body: JSON.stringify(user)
     })
-        .then(reponse => reponse.json())
-        .then(data => console.log(data))
-        .catch(err => console.log(err));
-});
+};
